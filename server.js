2bdcbdb3c0c868ec37d0a0ff636b17b647053aad
saumyadiphih7 <saumyadip@hih7.com>
2025-01-22 10:39:16 +0530

    cors added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import errorHandler from "./helper/errorHandler.js";
+import cors from "cors";
 
 
 dotenv.config();
@@ -10,6 +11,8 @@ dotenv.config();
 const app = express();
 
 connectDB();
+
+app.use(cors("*"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
